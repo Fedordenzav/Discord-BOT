79b0a873b041e3d348224f75858444a2c57cb5b3
feow)123 <143163743+Fedordenzav@users.noreply.github.com>
2024-07-04 18:13:25 +0300

    Create DBot.py

---
+++ DBot.py
@@ -0,0 +1,86 @@
+import random
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="/")
+
+
+@bot.command("info")
+async def command_info(ctx:commands.Context):
+    await ctx.send("Я классный бот!")
+
+
+
+@bot.command("predictions")
+async def command_predictions(ctx:commands.Context):
+    await ctx.send("У тебя сегодня будет хороший день!")  
+
+
+
+@bot.command("number")
+async def command_number(ctx:commands.Context):
+    await ctx.send("101") 
+
+
+@bot.command("plus")
+async def command_plus(ctx:commands.Context, a,b):
+    a = int(a)
+    b = int(b)
+    result = a + b
+    await ctx.send(result)
+
+
+@bot.command("minus")
+async def command_minus(ctx:commands.Context, a,b):
+    a = int(a)
+    b = int(b)
+    result = a - b
+    await ctx.send(result)
+
+
+
+@bot.command("multiply")
+async def command_multiply(ctx:commands.Context, a,b):
+    a = int(a)
+    b = int(b)
+    result = a * b
+    await ctx.send(result)
+
+@bot.command("tripple_plus")
+async def command_tripple_plus(ctx:commands.Context, a,b):
+    a = int(a)
+    b = int(b) 
+    c = int(c)
+    result = a + b + c
+    await ctx.send(result)
+
+
+
+
+
+
+@bot.command('guess')
+async def guess_number(ctx, user_guess: int):
+    await ctx.send(f'Твой ответ {user_guess}')
+    await ctx.send(f'Бот {random.randint(1, 10)}')
+
+    if random_number is None:
+        await ctx.send('Сначала начните новую игру командой $startgame.')
+        return
+
+    if user_guess == random_number:
+        await ctx.send(f':tada:Поздравляю! Вы угадали число {random_number}.')
+        random_number = None
+    elif user_guess < random_number:
+        await ctx.send('Ваше число меньше загаданного. Попробуйте еще раз!')
+    else:
+        await ctx.send('Ваше число больше загаданного. Попробуйте еще раз!')
+
+
+
+
+
+
+
+bot.run("MTI1NDQyMzc3MjA0MDEzNDgxNw.GTEjxp.IgNDUhhDYPGdIjQ6D_CjfF5YfbdHr1mW-w8E-k")
+
